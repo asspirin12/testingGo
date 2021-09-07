2f9c75c1a0a1850055042d4f5828bfb4179a6f77
Siarhei Kazlouski <asspirin12@gmail.com>
2021-09-07 23:57:13 +0300

    added calculator tests

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -1,1 +1,36 @@
 package calculator_test
+
+import (
+	"testing"
+	"testingGo/calculator"
+)
+
+type TestCase struct {
+	value    int
+	expected bool
+	actual   bool
+}
+
+func TestCalculateArmstrong(t *testing.T) {
+	testCase := TestCase{
+		value:    371,
+		expected: true,
+	}
+
+	testCase.actual = calculator.CalculateArmstrong(testCase.value)
+	if testCase.actual != testCase.expected {
+		t.Fail()
+	}
+}
+
+func TestNegativeCalculateArmstrong(t *testing.T) {
+	testCase := TestCase{
+		value:    3371,
+		expected: false,
+	}
+
+	testCase.actual = calculator.CalculateArmstrong(testCase.value)
+	if testCase.actual != testCase.expected {
+		t.Fail()
+	}
+}
